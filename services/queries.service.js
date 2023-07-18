17760b7e9ae947576b9776af30c649b624ae7c35
Charles <charlesnwoye2@gmail.com>
2023-07-18 19:59:08 +0100

    database connection established

--- services/queries.service.js
+++ services/queries.service.js
@@ -1,15 +1,13 @@
 require('dotenv').config();
 
 const databaseCreateQuery = `
-    CREATE DATABASE IF NOT EXISTS ${process.env.DB_DATABASE};
-    USE ${process.env.DB_DATABASE};
     CREATE TABLE IF NOT EXISTS farmers (
-      id INT PRIMARY KEY AUTO_INCREMENT,
+      id SERIAL PRIMARY KEY,
       first_name VARCHAR(255) NOT NULL,
       last_name VARCHAR(255) NOT NULL,
       age INT,
       address VARCHAR(255),
-      phone_number VARCHAR(20),
+      phone VARCHAR(20),
       crops JSON
     );
   `;
