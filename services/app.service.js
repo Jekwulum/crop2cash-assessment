8baab4e83319c24c0f349f76bb681e9f265f8c07
Charles <charlesnwoye2@gmail.com>
2023-07-19 18:29:50 +0100

    get farmer by age range

--- services/app.service.js
+++ services/app.service.js
@@ -1,6 +1,6 @@
 const AppService = {
   formatGetQuery(req, res, next) {
-    const { columns, first_name, last_name, age, address, phone_number, crops } = req.query;
+    const { columns, first_name, last_name, age, address, phone_number, crops, minAge, maxAge } = req.query;
     res.locals.columns = columns ? columns : '*';
 
     const conditions = [];
@@ -17,6 +17,14 @@ const AppService = {
         ? `crops @> '[${cropsArray[0]}]'`
         : `crops @> '[${cropsArray.join(', ')}]'`;
       conditions.push(condition);
+    }
+
+    if (minAge && maxAge) {
+      conditions.push(`age BETWEEN ${minAge} AND ${maxAge}`);
+    } else if (minAge) {
+      conditions.push(`age >= ${minAge}`);
+    } else if (maxAge) {
+      conditions.push(`age <= ${maxAge}`);
     }
 
     res.locals.conditionals = conditions.length > 0 ? conditions.join(' AND ') : ' ';
